4af5b6df38e0436b7f18644421c89c34cd14332c
Susmitha-56 <d.susmithatinu@gmail.com>
2019-08-05 14:48:46 +0530

    dummy

---
+++ src/main/java/com/dbs/emp_mgmt/App.java
@@ -0,0 +1,13 @@
+package com.dbs.emp_mgmt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
